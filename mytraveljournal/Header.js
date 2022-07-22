62f0edd18282988b03fd47da40056f5799f9e89c
alexyev <85699087+alexyev@users.noreply.github.com>
2022-07-22 09:49:20 -0400

    add mytraveljournal

---
+++ mytraveljournal/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header () {
+    return (
+        <header className='header-container'>
+            <img src='../images/world.png' className='header-img' />
+            <h1 className='header-title'>my travel journal.</h1>
+        </header>
+    )
+}
